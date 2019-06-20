be1c32fbf14fac15662c3746c6499fac90b390fd
Gihun Ham <progh2@gmail.com>
2019-06-21 01:08:05 +0900

    초기 커밋.

---
+++ ModalModalessForm/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ModalModalessForm
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btmModal = new System.Windows.Forms.Button();
+            this.btnModaless = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btmModal
+            // 
+            this.btmModal.Location = new System.Drawing.Point(12, 27);
+            this.btmModal.Name = "btmModal";
+            this.btmModal.Size = new System.Drawing.Size(186, 141);
+            this.btmModal.TabIndex = 1;
+            this.btmModal.Text = "모달창 띄우기";
+            this.btmModal.UseVisualStyleBackColor = true;
+            // 
+            // btnModaless
+            // 
+            this.btnModaless.Location = new System.Drawing.Point(204, 27);
+            this.btnModaless.Name = "btnModaless";
+            this.btnModaless.Size = new System.Drawing.Size(186, 141);
+            this.btnModaless.TabIndex = 4;
+            this.btnModaless.Text = "모달리스창 띄우기";
+            this.btnModaless.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 201);
+            this.Controls.Add(this.btnModaless);
+            this.Controls.Add(this.btmModal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Modal & Modaless 창 띄우기";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btmModal;
+        private System.Windows.Forms.Button btnModaless;
+    }
+}
+
